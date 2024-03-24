5cfde79cf016d84fd1634a5724e7b422c5453b94
Edric Liu <52363415+nyan-ko@users.noreply.github.com>
2024-03-24 13:25:03 -0400

    Update download.lua

--- download.lua
+++ download.lua
@@ -1,4 +1,6 @@
 shell.run("wget https://raw.githubusercontent.com/nyan-ko/lua_funny/main/ore_mining.lua ore_mining.lua")
 shell.run("wget https://raw.githubusercontent.com/nyan-ko/lua_funny/main/mining_utils.lua mining_utils.lua")
 shell.run("wget https://raw.githubusercontent.com/nyan-ko/lua_funny/main/utils.lua utils.lua")
+shell.run("wget https://raw.githubusercontent.com/nyan-ko/lua_funny/main/download.lua download.lua")
+shell.run("wget https://raw.githubusercontent.com/nyan-ko/lua_funny/main/clean.lua clean.lua")
 -- shell.run("wget ")
