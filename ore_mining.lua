c406d6184b00707878e5f1e699f91767b8188ab5
Edric Liu <52363415+nyan-ko@users.noreply.github.com>
2024-03-23 22:00:45 -0400

    Update ore_mining.lua

--- ore_mining.lua
+++ ore_mining.lua
@@ -1,63 +1,61 @@
-function main()
-    require "utils"
-    require "mining_utils"
+require "utils"
+require "mining_utils"
 
-    if arg[1] ~= nil then
-        for i=1,arg[1] do
-            up_refuel()
-        end
+if arg[1] ~= nil then
+    for i=1,arg[1] do
+        up_refuel()
     end
+end
 
-    local ready = true
-    local first = true
-    while ready do
-        if first then
-            first = false
-            if setup_digminer() > 0 then
-                break
-            end
-
-            CUR_MINER = peripheral.wrap("bottom")
-            CUR_MINER.start()
-            while check_digminer() do
-                os.sleep(5)
-            end
-            retrieve_digminer()
-            move_chunks(2)
-        else
-            if setup_digminer() > 0 then
-                break
-            end
-
-            CUR_MINER = peripheral.wrap("bottom")
-            CUR_MINER.start()
-
-            -- turn back
-            turtle.turnRight()
-            turtle.turnRight()
-
-            move_chunks(2)
-            try_down()
-            try_down()
-            try_down()
-            turtle.digDown()
-            try_down()
-            turtle.digDown()
-            -- should have just broken the cloader
-
-            try_up()
-            try_up()
-            try_up()
-            try_up()
-            turtle.turnRight()
-            turtle.turnRight()
-            move_chunks(2)
-
-            while check_digminer() do
-                os.sleep(5)
-            end
-            retrieve_digminer()
-            move_chunks(2)
+local ready = true
+local first = true
+while ready do
+    if first then
+        first = false
+        if setup_digminer() > 0 then
+            break
         end
+
+        CUR_MINER = peripheral.wrap("bottom")
+        CUR_MINER.start()
+        while check_digminer() do
+            os.sleep(5)
+        end
+        retrieve_digminer()
+        move_chunks(2)
+    else
+        if setup_digminer() > 0 then
+            break
+        end
+
+        CUR_MINER = peripheral.wrap("bottom")
+        CUR_MINER.start()
+
+        -- turn back
+        turtle.turnRight()
+        turtle.turnRight()
+
+        move_chunks(2)
+        try_down()
+        try_down()
+        try_down()
+        turtle.digDown()
+        try_down()
+        turtle.digDown()
+        -- should have just broken the cloader
+
+        try_up()
+        try_up()
+        try_up()
+        try_up()
+        turtle.turnRight()
+        turtle.turnRight()
+        move_chunks(2)
+
+        while check_digminer() do
+            os.sleep(5)
+        end
+        retrieve_digminer()
+        move_chunks(2)
     end
 end
