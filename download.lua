c02feefe06cfa2479cf3306ce9278bfb1ad18646
Edric Liu <52363415+nyan-ko@users.noreply.github.com>
2024-03-23 21:47:28 -0400

    Add files via upload

---
+++ download.lua
@@ -0,0 +1,4 @@
+shell.run("wget https://raw.githubusercontent.com/nyan-ko/lua_funny/main/ore_mining.lua ore_mining.lua")
+shell.run("wget https://raw.githubusercontent.com/nyan-ko/lua_funny/main/mining_utils.lua mining_utils.lua")
+shell.run("wget https://raw.githubusercontent.com/nyan-ko/lua_funny/main/utils.lua utils.lua")
+-- shell.run("wget ")
