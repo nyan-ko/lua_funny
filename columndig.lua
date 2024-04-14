c9be166f5062feeaac9721771eee2a32032a93cb
nyan-ko <dumbstupidcatboy@gmail.com>
2024-04-14 17:47:04 -0400

    dig while moving

--- columndig.lua
+++ columndig.lua
@@ -3,17 +3,31 @@ require "utils"
 Blocks = {}
 Blocks.STOP = "minecraft:bedrock"
 
-local dug = 0
-while (true) do
-    local s, d = turtle.inspectDown()
-    if s and d.name == Blocks.STOP then
-        break
-    end
-    turtle.digDown()
-    down_refuel()
-    dug = dug + 1
+local move = 0
+if arg[1] ~= nil then
+    move = arg[1]
 end
 
-for i=1,dug do
-    up_refuel()
+while move >= 0 do
+    local dug = 0
+    while (true) do
+        local s, d = turtle.inspectDown()
+        if s and d.name == Blocks.STOP then
+            break
+        end
+        turtle.digDown()
+        down_refuel()
+        dug = dug + 1
+    end
+
+    for i=1,dug do
+        turtle.digUp()
+        up_refuel()
+    end
+
+    move = move - 1
+    if move > 0 then
+        turtle.dig()
+        forward_refuel()
+    end
 end
